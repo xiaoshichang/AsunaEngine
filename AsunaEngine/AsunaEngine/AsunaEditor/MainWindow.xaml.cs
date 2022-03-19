b5d2132ec230026735077587637e804eedd97b96
xiaoshichang <511680777@qq.com>
2022-03-20 00:17:48 +0800

    Asuna Engine project basic operation.

--- AsunaEngine/AsunaEngine/AsunaEditor/MainWindow.xaml.cs
+++ AsunaEngine/AsunaEngine/AsunaEditor/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using AsunaEditor.Structures;
+using Microsoft.Win32;
 
 namespace AsunaEditor
 {
@@ -23,6 +25,55 @@ namespace AsunaEditor
         public AsunaEditorMainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MenuItem_Click_NewProject(object sender, RoutedEventArgs e)
+        {
+            var result = AsunaProject.Create("C:\\Users\\xiao\\Desktop\\AsunaClientProjectDemo", "0.0.1");
+            if (result != AsunaProjectCreateResult.Success)
+            {
+                string messageBoxText = $"{result}!";
+                string caption = "Create Project Error";
+                MessageBox.Show(messageBoxText, caption, MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+        }
+
+        private void MenuItem_Click_OpenProject(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = $"AsunaProject File|{AsunaProject.ProjectFileName}";
+            openFileDialog.Multiselect = false;
+            if (openFileDialog.ShowDialog() == true)
+            {
+                var projectFile = openFileDialog.FileName;
+                var result = AsunaProject.Load(projectFile);
+                if (result != AsunaProjectLoadResult.Success)
+                {
+                    string messageBoxText = $"{result}";
+                    string caption = "Open Project Error";
+                    MessageBox.Show(messageBoxText, caption, MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+                ActionResult.Text = "Open Project OK!";
+                Title = $"AsunaEditor - {projectFile}";
+            }
+        }
+
+        private void MenuItem_Click_SaveProject(object sender, RoutedEventArgs e)
+        {
+            if (AsunaProject.Current == null)
+            {
+                string messageBoxText = "No Project opened!";
+                string caption = "Save Project Error";
+                MessageBox.Show(messageBoxText, caption, MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+        }
+
+        private void MenuItem_Click_ExitEditor(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
---
+++ AsunaEngine/AsunaEngine/AsunaEditor/Structures/AsunaProject.cs
@@ -0,0 +1,67 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace AsunaEditor.Structures
+{
+    public enum AsunaProjectCreateResult
+    {
+        Success = 0,
+        AllreadyExist = 1
+    }
+
+    public enum AsunaProjectLoadResult
+    {
+        Success = 0
+    }
+
+    [DataContract]
+    public class AsunaProject
+    {
+        public FileStream ProjectFile;
+        public string ProjectPath { get; set; }
+
+        [DataMember]
+        public string ProjectVersion { get; set; }
+
+        public void Save()
+        {
+            ProjectFile.Seek(0, SeekOrigin.Begin);
+            var content = JsonConvert.SerializeObject(this, Formatting.Indented);
+            var data = Encoding.Default.GetBytes(content);
+            ProjectFile.Write(data, 0, data.Length);
+        }
+
+        public static AsunaProjectLoadResult Load(string projectFile)
+        {
+            var content = File.ReadAllText(projectFile);
+            var project = JsonConvert.DeserializeObject<AsunaProject>(content);
+            Current = project;
+            return AsunaProjectLoadResult.Success;
+        }
+
+        public static AsunaProjectCreateResult Create(string projectRoot, string projectVersion)
+        {
+            var project = new AsunaProject();
+            var projectFilePath = Path.Combine(projectRoot, ProjectFileName);
+
+            if (File.Exists(projectFilePath))
+            {
+                return AsunaProjectCreateResult.AllreadyExist;
+            }
+
+            project.ProjectFile = File.Open(projectFilePath, FileMode.CreateNew);
+            project.ProjectPath = projectRoot;
+            project.ProjectVersion = projectVersion;
+            project.Save();
+            return AsunaProjectCreateResult.Success;
+        }
+
+        public const string ProjectFileName = "AsunaProject.json";
+        public static AsunaProject Current = null;
+
+    }
+
+    
+}
